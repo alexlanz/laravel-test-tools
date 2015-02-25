f1035bc8a7e7044eb06929d0e81dc1e44d818690
Alex Lanz <alex@krumer.it>
2015-02-25 21:29:36 +0100

    Add more methods for Api trait

--- src/ApiTestToolsTrait.php
+++ src/ApiTestToolsTrait.php
@@ -14,12 +14,33 @@ trait ApiTestToolsTrait {
      * @param null $content
      * @return \Illuminate\Http\Response
      */
-    public function call($method, $uri, $parameters = [], $cookies = [], $files = [], $server = [], $content = null)
+    public function callApi($method, $uri, $parameters = [], $cookies = [], $files = [], $server = [], $content = null)
     {
         // Set headers for a valid ajax request
         $server['HTTP_X-Requested-With'] = 'XMLHttpRequest';
 
-        return parent::call($method, $uri, $parameters, $cookies, $files, $server, $content);
+        return $this->call($method, $uri, $parameters, $cookies, $files, $server, $content);
+    }
+
+    /**
+     * Call the given URI as a Api/Ajax call and return the json decoded content.
+     *
+     * @param string $method
+     * @param string $uri
+     * @param array $parameters
+     * @param array $cookies
+     * @param array $files
+     * @param array $server
+     * @param null $content
+     * @return \Illuminate\Http\Response
+     */
+    public function callApiAndReturnJsonDecodedContent($method, $uri, $parameters = [], $cookies = [], $files = [], $server = [], $content = null)
+    {
+        $response = $this->callApi($method, $uri, $parameters, $cookies, $files, $server, $content);
+
+        $content = $response->getContent();
+
+        return json_decode($content);
     }
 
 }
